1885806439ba9af4e93a1e212044c77a94a47200
CcharlotteHh <90309917+CcharlotteHh@users.noreply.github.com>
2022-01-20 13:28:12 +0100

    Add files via upload

---
+++ eindopdrachtmetstap3pleasebesaved/eindopdrachtmetstap3/assets/js/productpage.js
@@ -0,0 +1,39 @@
+function swap1() {
+    var cat1 = document.getElementById('cat1');
+        cat2 = document.getElementById('cat2');
+        cat1SRC = cat1.src;
+        cat2SRC = cat2.src;
+    cat1.src = cat2SRC
+    cat2.src = cat1SRC    
+}
+
+function swap2() {
+    var cat1 = document.getElementById('cat1');
+        cat3 = document.getElementById('cat3');
+        cat1SRC = cat1.src;
+        cat3SRC = cat3.src;
+    cat1.src = cat3SRC
+    cat3.src = cat1SRC    
+}
+
+function swap3() {
+    var cat1 = document.getElementById('cat1');
+        cat4 = document.getElementById('cat4');
+        cat1SRC = cat1.src;
+        cat4SRC = cat4.src;
+    cat1.src = cat4SRC
+    cat4.src = cat1SRC    
+}
+
+function swap4() {
+    var cat1 = document.getElementById('cat1');
+        cat5 = document.getElementById('cat5');
+        cat1SRC = cat1.src;
+        cat5SRC = cat5.src;
+    cat1.src = cat5SRC
+    cat5.src = cat1SRC    
+}
+
+
+
+
